2d4b4edb8439789d53f2c75a07e72633020d8af2
ohagip <ohagiohagiohagi@gmail.com>
2021-07-07 19:33:57 +0900

    Fix live reload.

--- webpack/dev.js
+++ webpack/dev.js
@@ -5,13 +5,12 @@ const commonConfiguration = require('./common.js')
 module.exports = merge(
   commonConfiguration,
   {
+    target: 'web', // live reloadが動かないバグ対応 https://github.com/webpack/webpack-dev-server/issues/2758
     mode: 'development',
     devtool: 'source-map',
     devServer: {
       host: '0.0.0.0',
       useLocalIp: true,
-      contentBase: path.resolve(__dirname, '../public'),
-      watchContentBase: true,
       open: true,
       // openPage: './sample',
       // stats: 'minimal', // 出力ログをへらす
